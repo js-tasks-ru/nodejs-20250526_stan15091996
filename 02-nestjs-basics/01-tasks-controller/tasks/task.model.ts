7330f6eb5a5cd854f49e72a846c280e1df600f5a
Dmitry <stan15091996@mail.ru>
2025-06-01 12:48:52 +0300

    added module-2-1 homework

--- 02-nestjs-basics/01-tasks-controller/tasks/task.model.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/task.model.ts
@@ -1,12 +1,17 @@
+import { IsEmail, IsNotEmpty, IsEnum } from 'class-validator';
+
 export enum TaskStatus {
   PENDING = "pending",
   IN_PROGRESS = "in_progress",
   COMPLETED = "completed",
 }
 
-export interface Task {
+export class Task {
   id?: string;
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
+  @IsEnum(TaskStatus)
   status: TaskStatus;
 }
--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,58 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { Task } from "./task.model";
+import { timeStamp } from "console";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task {
+    const taskIndex = this.getTaskIndexById(id);
 
-  createTask(task: Task): Task {}
+    if (taskIndex < 0) throw new NotFoundException(`task with id ${id} not found`);
 
-  updateTask(id: string, update: Task): Task {}
+    return this.tasks[taskIndex];
+  }
 
-  deleteTask(id: string): Task {}
+  createTask(task: Task): Task {
+    const currentId: string = this.tasks[this.tasks.length - 1]?.id;
+    const newTask: Task = {
+      ...task,
+      id: currentId ? String(Number(currentId) + 1) : String(this.tasks.length + 1),
+    }
+
+    this.tasks.push(newTask);
+
+    return newTask;
+  }
+
+  updateTask(id: string, update: Task): Task {
+    const taskIndex = this.getTaskIndexById(id);
+
+    if (taskIndex < 0) throw new NotFoundException(`task with id ${id} not found`);
+
+    for (let key in update) {
+      if (key !== 'id') {
+        this.tasks[taskIndex][key] = update[key];
+      }
+    }
+    
+    return this.tasks[taskIndex];
+  }
+
+  deleteTask(id: string): Task {
+    const taskIndex = this.getTaskIndexById(id);
+
+    if (taskIndex < 0) throw new NotFoundException(`task with id ${id} not found`);
+
+    return this.tasks.splice(taskIndex, 1)[0];
+  }
+
+  private getTaskIndexById(id: string): number {
+    return this.tasks.findIndex((task: Task) => task.id === id);
+  }
 }

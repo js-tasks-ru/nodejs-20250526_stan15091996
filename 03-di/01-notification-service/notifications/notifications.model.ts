d64dc9ec9626f8ec61c700e0a2c6c511cb20a098
Dmitry <stan15091996@mail.ru>
2025-06-07 16:01:52 +0300

    added module 3 homework

---
+++ 03-di/01-notification-service/notifications/notifications.model.ts
@@ -0,0 +1,4 @@
+export interface INotificationsOption {
+  senderEmail: string,
+  smsGateway: string,
+}
--- 03-di/01-notification-service/notifications/notifications.service.ts
+++ 03-di/01-notification-service/notifications/notifications.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Inject, Injectable } from "@nestjs/common";
+import { INotificationsOption } from "./notifications.model";
+import { readFileSync,  writeFileSync} from "node:fs";
+import { json } from "node:stream/consumers";
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+  constructor(@Inject('NOTIFICATION_OPTIONS') private readonly options: INotificationsOption){}
+  sendEmail(to: string, subject: string, message: string) {
+    if (!to || !subject || !message) throw new BadRequestException('Некорректные данные для отправки E-mail');
+
+    console.log(this.options.senderEmail);
+    const log = `Email sent to ${to}: ${subject} Вы назначены ответственным за задачу: "${message}"`;
+    console.log(log)
+    this.writeLogs(log);
+  }
+
+  sendSMS(to: string, message: string) {
+    if (!to || !message) throw new BadRequestException('Некорректные данные для отправки SMS');
+    console.log(this.options.smsGateway);
+    console.log(`SMS sent to ${to}: ${message}`)
+    this.writeLogs('');
+  }
+
+  private writeLogs(log: string) {
+    const logs = JSON.parse(readFileSync('./notifications/logs.json', 'utf-8'));
+    logs.logs.push(log);
+    writeFileSync('./notifications/logs.json', JSON.stringify(logs));
+  }
+}

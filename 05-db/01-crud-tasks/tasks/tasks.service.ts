4fe804d157dea9b740e1740c769ccae78cce66a0
Dmitry <stan15091996@mail.ru>
2025-06-13 13:37:44 +0300

    added module 5 homework

--- 05-db/01-crud-tasks/tasks/tasks.service.ts
+++ 05-db/01-crud-tasks/tasks/tasks.service.ts
@@ -1,16 +1,41 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { UpdateTaskDto } from "./dto/update-task.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Task } from "./entities/task.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class TasksService {
-  create(createTaskDto: CreateTaskDto) {}
+  constructor(@InjectRepository(Task) private taskRepository: Repository<Task>){}
+  async create(createTaskDto: CreateTaskDto) {
+    const task = new Task();
+    task.title = createTaskDto.title;
+    task.description = createTaskDto.description;
 
-  async findAll() {}
+    return await this.taskRepository.save(task);
+  }
 
-  async findOne(id: number) {}
+  async findAll() {
+    return await this.taskRepository.find();
+  }
 
-  async update(id: number, updateTaskDto: UpdateTaskDto) {}
+  async findOne(id: number) {
+    return await this.taskRepository.find({where: {id}});
+  }
 
-  async remove(id: number): Promise<void> {}
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    await this.taskRepository.update(id, updateTaskDto);
+    return await this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    const currentTask = await this.findOne(id);
+
+    if (!currentTask.length) {
+      throw new BadRequestException('Задачи с данным id не существует');
+    }
+
+    await this.taskRepository.delete(id);
+  }
 }

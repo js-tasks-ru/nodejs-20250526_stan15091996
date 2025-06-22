2d3ac1626c4d83c71fa4b1ed6d2a59cb64686498
Dmitry <stan15091996@mail.ru>
2025-06-22 12:50:46 +0300

    fixed components homework

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,18 +1,21 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
 import { Request, Response } from 'express';
 import {appendFileSync} from 'fs';
 
-@Catch(HttpException)
+@Catch()
 export class HttpErrorFilter implements ExceptionFilter {
   catch(exception:  HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
     const message = exception.message;
     console.log(exception);
     const timestamp = new Date().toISOString();
 
-    appendFileSync('./errors.logs', `[${timestamp}] ${status} - ${message}`);
+    appendFileSync('errors.log', `[${timestamp}] ${status} - ${message}\n`);
 
     response
       .status(status)

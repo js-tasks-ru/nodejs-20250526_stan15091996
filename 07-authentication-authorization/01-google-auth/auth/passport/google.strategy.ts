8e91b7cef1b868284a3f4f1a6e517b68fd8060e1
Dmitry <stan15091996@mail.ru>
2025-06-29 15:35:18 +0300

    added 7 module homework

--- 07-authentication-authorization/01-google-auth/auth/passport/google.strategy.ts
+++ 07-authentication-authorization/01-google-auth/auth/passport/google.strategy.ts
@@ -1,8 +1,37 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy } from "passport-google-oauth20";
 import { Injectable } from "@nestjs/common";
+import { UsersService } from "../../users/users.service";
+import { User } from "users/entities/user.entity";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-  async validate(accessToken: string, refreshToken: string, profile: Profile) {}
+    constructor(
+    private usersService: UsersService,
+  ) {
+    super({
+      clientID: '',
+      clientSecret: '',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      scope: ['profile', 'email', 'openid'],
+    });
+  }
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+    
+
+    let user = await this.usersService.findOne(profile.username);
+    if (!user) {
+      const newProfile: User = {
+        id: profile.id,
+        displayName: profile.displayName,
+        avatar: profile.photos[0].value,
+      }
+      user = await this.usersService.create(newProfile);
+    }
+
+    return user;
+  }
 }
